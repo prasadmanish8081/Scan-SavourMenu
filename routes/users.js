ddec36671b63aa9d70e77da90bda3325b45aac5b
Manish Prasad <136253028+prasadmanish8081@users.noreply.github.com>
2024-07-28 13:39:33 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/qrmenuscan");
+const dbURI = process.env.MONGODB_URI;
 
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log('MongoDB connection error:', err));
 
 
 const customerSchema = mongoose.Schema({
